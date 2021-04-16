21cf31d29b53521da1ba23c563921c439f0cb33c
IsseNisse <isak.vindeln@gmail.com>
2021-04-16 21:52:56 +0200

    created model, controller and migration for pastry with 7 restfull controller actions

---
+++ app/Http/Controllers/PastryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PastryController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function show() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+
+    }
+}
---
+++ database/migrations/2021_04_16_193251_create_pastries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePastriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastries', function (Blueprint $table) {
+            $table->id();
+            $table->string("pastry_name")
+            $table->string("img_path")
+            $table->string("price")
+            $table->text("description")
+            $table->text("ingredients")
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pastries');
+    }
+}
