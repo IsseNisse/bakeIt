8f29bc06ec9723b72e0911bc0f8034c3dff7c024
IsseNisse <isak.vindeln@gmail.com>
2021-04-25 16:08:13 +0200

    styled the create pastry form

--- resources/views/pastry/create.blade.php
+++ resources/views/pastry/create.blade.php
@@ -1,19 +1,26 @@
 <x-master>
-    <form class="container" method="POST" action="/">
-        @csrf
-
-        <label for="name">Namn:</label>
-        <input type="text" name="name" id="name">
-
-        <label for="name">Image Path:</label>
-        <input type="text" name="name" id="name">
-
-        <label for="name">Price:</label>
-        <input type="text" name="name" id="name">
-
-        <label for="name">Description:</label>
-        <input type="text" name="name" id="name">
-
-        <button class="bg-pink-600 p-2 rounded-lg" type="submit">Create</button>
-    </form>
+    <div class="flex items-center justify-center mt-5">
+        <form method="POST" action="/">
+            @csrf
+    
+            <div class="flex flex-col mb-4">
+                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="name">Namn:</label>
+                <input class="border py-2 px-3 text-grey-darkest" type="text" name="name" id="name">
+            </div>
+            <div class="flex flex-col mb-4">
+                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="image">Bild</label>
+                <input class=""type="file" name="image" id="image">
+            </div>
+            <div class="flex flex-col mb-4">
+                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="price">Pris:</label>
+                <input class="border py-2 px-3 text-grey-darkest" type="text" name="price" id="price">    
+            </div>
+            <div class="flex flex-col mb-4">
+                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="description">Beskrivning:</label>
+                <input class="border py-2 px-3 text-grey-darkest" type="text" name="description" id="description">    
+            </div>
+    
+            <button class="bg-pink-600 p-2 rounded-lg" type="submit">Create</button>
+        </form>
+    </div>
 </x-master>
