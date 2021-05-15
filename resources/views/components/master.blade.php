9dab760f0d8085fd9bc058d3f7a652a076c319a9
IsseNisse <isak.vindeln@gmail.com>
2021-05-16 00:57:57 +0200

    made a sticky footer

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -10,7 +10,9 @@
 <body>
     <nav class="flex items-center justify-between flex-wrap p-6 border-b-2 border-pink-600">
         <div class="flex items-center flex-shrink-0 text-black mr-6">
-            <h1 class="text-4xl">BakeIt</h1>
+            <a href="/" class="text-black italic">
+                <h1 class="text-4xl">BakeIt</h1>
+            </a>
         </div>
 
         <div class="flex font-medium">
@@ -39,5 +41,26 @@
 
     {{ $slot }}
 
+
+    <footer class="fixed w-full flex items-center justify-center left-0 bottom-0 bg-white gap-6 mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FA00FF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="16" height="16" rx="4" />
+            <circle cx="12" cy="12" r="3" />
+            <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-snapchat" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FA00FF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M16.882 7.842a4.882 4.882 0 0 0 -9.764 0c0 4.273 -.213 6.409 -4.118 8.118c2 .882 2 .882 3 3c3 0 4 2 6 2s3 -2 6 -2c1 -2.118 1 -2.118 3 -3c-3.906 -1.709 -4.118 -3.845 -4.118 -8.118zm-13.882 8.119c4 -2.118 4 -4.118 1 -7.118m17 7.118c-4 -2.118 -4 -4.118 -1 -7.118" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-tiktok" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FA00FF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M9 12a4 4 0 1 0 4 4v-12a5 5 0 0 0 5 5" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FA00FF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+        </svg>
+    </footer>
 </body>
 </html>
--- resources/views/pastry/show.blade.php
+++ resources/views/pastry/show.blade.php
@@ -2,8 +2,8 @@
     <div class="flex items-center justify-center mt-20">
         <div class="grid w-3/4 lg:grid-flow-col lg:auto-cols-auto lg:grid-rows-2 lg:w-2/4">
             <img class="w-full h-96 object-cover lg:pr-8" src="{{ $pastry->img_path }}" alt="">
-            <div class="text-center lg:pr-8">
-                <h1 class="m-2 text-4xl">{{ $pastry->pastry_name }}</h1>
+            <div class=" lg:pr-8">
+                <h1 class="m-2 text-center text-4xl">{{ $pastry->pastry_name }}</h1>
                 <p class="mx-4">{{ $pastry->description }}</p>
             </div>
             <div class="lg:row-span-full border-l-2 border-pink-600">
